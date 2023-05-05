ed2b3ece810f55bfd21622950f497aa8cfc75467
Dubymar Tollinchi <dubymarjtr@gmail.com>
2023-05-05 11:57:26 -0400

    create Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='dib ba bg-washed-blue br3 ma3 pa3 tc shadow-5 grow'>
+            <div>
+                <h2>Pokemon 1</h2>
+                <p>some info about it</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Card;
