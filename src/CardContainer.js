17737abb06e51fcee6a675aceb7c929d4b423ba4
Dubymar Tollinchi <dubymarjtr@gmail.com>
2023-05-09 12:22:06 -0400

    create Card Container and replace it on index.js

---
+++ src/CardContainer.js
@@ -0,0 +1,14 @@
+import React, { useState} from 'react';
+import Card from './Card.js';
+import axios from 'axios';
+
+const CardContainer = () => {
+
+    return (
+        <>
+            <Card />
+        </>
+    )
+}
+
+export default CardContainer;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card.js';
+import CardContainer from './CardContainer.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <div>
-    <Card />
-    <Card />
-    <Card />
+    <CardContainer />
   </div>
   </React.StrictMode>
 );
